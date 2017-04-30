f76ad3601b458c0faa02c4ab157a1f51f8323102
xiaotian <952822399@qq.com>
2017-04-30 20:03:12 +0800

    Cool women
    Cool women

--- js/style.js
+++ js/style.js
@@ -1,3 +1,39 @@
 /**
  * Created by Administrator on 2017/4/26.
  */
+
+
+window.onload = function(){
+    var aPrevious = document.getElementById("aNext");
+    var aNext = document.getElementById("aPrevious");
+    var aImage = document.getElementById("aImage");
+    var aImages = document.getElementById("aImages");
+    var aImageArr = ["img/hMainFourContentCenterCenterRightFirure.png", "img/hMainFourContentCenterCenterRightFirure1.png", "img/hMainFourContentCenterCenterRightFirure2.png"];
+    var aImageArrs = ["img/hMainFourContentRightFirure.png", "img/hMainFourContentRightFirure1.png", "img/hMainFourContentRightFirure2.png"];
+    var num = 0;
+
+
+    function fnTab() {
+        aImage.src = aImageArr[num];
+        aImages.src = aImageArrs[num];
+    };
+
+    aNext.onclick = function(){
+        num++;
+        if(num == aImageArr.length ){
+            num = 0;
+        }
+       fnTab();
+        console.log(num);
+    };
+
+    aPrevious.onclick = function(){
+        num--;
+
+        if(num == -1){
+            num = aImageArr.length-1;
+        }
+        fnTab();
+        console.log(num);
+    };
+};
